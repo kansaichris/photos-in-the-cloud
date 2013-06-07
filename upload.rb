1a1bf72158c81992b4b6cb6f91217aa1b6043018
Christopher Frederick <chris@chrisfrederick.net>
2013-06-07 14:27:05 +0900

    Add a command-line option for the Amazon S3 region

--- upload.rb
+++ upload.rb
@@ -27,6 +27,7 @@ opts = Trollop::options do
     version "S3 Photo Manager 0.0.1 (c) 2013 Christopher Frederick"
     opt :file, "File to upload", :type => :string
     opt :bucket, "Amazon S3 bucket", :type => :string
+    opt :region, "Region where the bucket is located. By default, this is US Standard. Use 'us-west-2' for US West (Oregon), 'us-west-1' for US West (Northern California), 'eu' for EU (Ireland), 'ap-southeast-1' for Asia Pacific (Singapore), 'ap-southeast-2' for Asia Pacific (Sydney), 'ap-northeast-1' for Asia Pacific (Tokyo), and 'sa-east-1' for South America (Sao Paulo)", :type => :string
     opt :path, "Photo path (filename prefix) in the specified bucket", :type => :string
     opt :aws_key_id, "Access key ID for Amazon Web Services", :type => :string
     opt :aws_secret_key, "Secret access key for Amazon Web Services", :type => :string
