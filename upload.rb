6597eb0968b490dac00e3f91f409d27e1df8e939
Christopher Frederick <chris@chrisfrederick.net>
2013-06-07 14:33:08 +0900

    Use :required to enforce required Trollop options

--- upload.rb
+++ upload.rb
@@ -25,31 +25,25 @@ require 'trollop'
 # opts[:bucket] holds the specified bucket, etc.
 opts = Trollop::options do
     version "S3 Photo Manager 0.0.1 (c) 2013 Christopher Frederick"
-    opt :file, "File to upload", :type => :string
-    opt :bucket, "Amazon S3 bucket", :type => :string
-    opt :region, "Region where the bucket is located. By default, this is US Standard. Use 'us-west-2' for US West (Oregon), 'us-west-1' for US West (Northern California), 'eu' for EU (Ireland), 'ap-southeast-1' for Asia Pacific (Singapore), 'ap-southeast-2' for Asia Pacific (Sydney), 'ap-northeast-1' for Asia Pacific (Tokyo), and 'sa-east-1' for South America (Sao Paulo)", :type => :string
-    opt :path, "Photo path (filename prefix) in the specified bucket", :type => :string
-    opt :aws_key_id, "Access key ID for Amazon Web Services", :type => :string
-    opt :aws_secret_key, "Secret access key for Amazon Web Services", :type => :string
+
+    opt :file, "File to upload",
+        :type => :string, :required => true
+
+    opt :bucket, "Amazon S3 bucket",
+        :type => :string, :required => true
+
+    opt :region, "Region where the bucket is located. By default, this is US Standard. Use 'us-west-2' for US West (Oregon), 'us-west-1' for US West (Northern California), 'eu' for EU (Ireland), 'ap-southeast-1' for Asia Pacific (Singapore), 'ap-southeast-2' for Asia Pacific (Sydney), 'ap-northeast-1' for Asia Pacific (Tokyo), and 'sa-east-1' for South America (Sao Paulo)",
+        :type => :string
+
+    opt :path, "Photo path (filename prefix) in the specified bucket",
+        :type => :string, :required => true
+
+    opt :aws_key_id, "Access key ID for Amazon Web Services",
+        :type => :string, :required => true
+
+    opt :aws_secret_key, "Secret access key for Amazon Web Services",
+        :type => :string, :required => true
 end
-
-# --file must be specified
-Trollop::die :file, "must be specified" unless opts[:file]
-
-# --file must indicate a valid file
-Trollop::die :file, "must be a valid file" unless File.exists?(opts[:file])
-
-# --bucket must be specified
-Trollop::die :bucket, "must be specified" unless opts[:bucket]
-
-# --path must be specified
-Trollop::die :path, "must be specified" unless opts[:path]
-
-# --aws-key-id must be specified
-Trollop::die :aws_key_id, "must be specified" unless opts[:aws_key_id]
-
-# --aws-secret-key must be specified
-Trollop::die :aws_secret_key, "must be specified" unless opts[:aws_secret_key]
 
 # DEBUG: Print the opts hash
 p opts
