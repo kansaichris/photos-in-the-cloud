559847deb3f757c258616449aa502abbda2cd26f
Christopher Frederick <chris@chrisfrederick.net>
2013-06-13 14:26:46 +0900

    Clean up upload.rb's "Main" section
    Remove unnecessary code and add spaces for legibility.

--- upload.rb
+++ upload.rb
@@ -141,23 +141,17 @@ end
 ################################################################################
 
 put_request = PUTObject.new(opts[:file], opts[:bucket], opts[:region], opts[:path], current_time, opts[:aws_key_id], opts[:aws_secret_key])
+
+puts
 puts "DEBUG: String to sign:"
 puts "----------------------"
 puts put_request.string_to_sign
 puts "----------------------"
 puts
-=begin
-hash = Digest::MD5.base64digest put_request.body
-puts "MD5 hash of request body: #{hash}"
-puts "Class of request body: #{put_request.body.class.name}"
-puts "Encoding of request body: #{put_request.body.encoding.name}"
-puts "Number of bytes in request body: #{put_request.body.bytesize}"
-puts "First character in request body: #{put_request.body.getbyte(4)}"
-# IO.write("image.jpg", put_request.body.force_encoding("UTF-8"))
-=end
+
+put_request.print_headers
 
 response = put_request.get_response
-# response = http.request(put_request)
 
 puts "Response code: #{response.code}"
 puts "Response message: #{response.message}"
@@ -172,6 +166,7 @@ end
 puts "------------------------------------"
 puts
 puts response.body
+
 =begin
 puts "DEBUG: Keys in the response:"
 puts "----------------------------"
@@ -184,60 +179,12 @@ end
 puts "----------------------------"
 puts
 =end
+
 =begin
-# Get a list of the files in the specified bucket
-my_bucket = Bucket.new(opts[:bucket], opts[:region])
-files = my_bucket.get_files_for_key(opts[:aws_key_id], opts[:aws_secret_key])
-
-# Print all of the filenames
-unless files.empty?
-    puts "Received the following filenames:"
-    count = 0
-    files.each do |filename|
-        count = count + 1
-        puts "#{count}: #{filename}"
-    end
-end
-
-# Open the file & determine its MIME type by reading its magic header ##########
-# For more information, see
-# http://www.garykessler.net/library/file_sigs.html
-filename = opts[:file]
-file = File.open(filename, "rb")
-
-# I'm currently checking for the following two magic headers:
-# FF D8 FF E0 xx xx 4A 46 49 46 00 - JPEG/JFIF graphics file
-# FF D8 FF E1 xx xx 45 78 69 66 00 - Digital camera JPG using EXIF
-file_type = ''
-jpg_regexp = Regexp.new("\xff\xd8\xff(\xe0|\xe1).{2}JFIF".force_encoding("binary"))
-case IO.read(filename, 10)
-when /^#{jpg_regexp}/
-    file_type = 'image/jpeg'
-end
-
-# Abort if the file doesn't have one of the magic headers listed above
-# TODO: Add more file types as necessary
-if file_type.empty?
-    puts "#{filename} does not appear to be a supported file type"
-    puts "This script currently only supports JPEG files"
-    abort
-end
-
 # Calculate the file's SHA-1 hash ##############################################
 file_size = file.size
 file_contents = file.read
 sha1_hash = Digest::SHA1.hexdigest "blob #{file_size}\0#{file_contents}"
-
-# Calculate the file's MD5 hash ################################################
-md5_hash = Digest::MD5.base64digest file_contents
-=end
-=begin
-file_name = sha1_hash
-string_to_sign = "PUT
-#{md5_hash}
-#{file_type}
-#{time_string}
-/#{bucket_name}/#{folder_name}/#{file_name}"
 =end
 
 =begin
@@ -259,6 +206,3 @@ signature3 = Base64.encode64(digest3)
 puts "DEBUG: The calculated signature is #{signature2}\n"
 puts "DEBUG: The calculated signature is #{signature3}\n"
 =end
-
-# Close the file
-# file.close
