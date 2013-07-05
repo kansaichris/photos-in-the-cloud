270a89dcb1a74c651e300b1a4aec07e551ccbc63
Christopher Frederick <chris@chrisfrederick.net>
2013-07-05 10:23:01 +0900

    Add the Worker class for multithreaded processing

---
+++ worker.rb
@@ -0,0 +1,45 @@
+class Worker
+
+    def initialize queue
+        @idle_state = false
+        @exit_state = false
+        @queue = queue
+        @idle_mutex = Mutex.new
+        @exit_mutex = Mutex.new
+        poll
+    end
+
+    # Pull a Proc from a queue and process it
+    def poll
+        @thread = Thread.new do
+            until done?
+                while @queue.empty?
+                    set_idle true
+                    sleep 1
+                    exit 0 if done?
+                end
+                set_idle false
+                job = @queue.pop
+                job.call
+            end
+        end
+    end
+
+    def done?
+        @exit_mutex.synchronize { @exit_state }
+    end
+
+    def shut_down
+        @exit_mutex.synchronize { @exit_state = true }
+        @thread.join
+    end
+
+    def idle?
+        @idle_mutex.synchronize { @idle_state }
+    end
+
+    def set_idle state
+        @idle_mutex.synchronize { @idle_state = state }
+    end
+
+end
