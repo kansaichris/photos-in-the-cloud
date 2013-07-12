eb912a19639a8352a482407a43db114ef486d731
Christopher Frederick <chris@chrisfrederick.net>
2013-07-12 14:20:11 +0900

    Move option processing into view_exif_data.rb
    Rather than share the (not-really-common) code in options.rb, move
    tool-specific CLI options into the tool's main code.

--- view_exif_data.rb
+++ view_exif_data.rb
@@ -1,14 +1,42 @@
 #!/usr/bin/env rvm 1.9.3 do ruby
 
-require_relative 'options'
 require_relative 'image'
+
+require 'trollop'
+
+################################################################################
+# COMMAND-LINE OPTIONS
+################################################################################
+
+def process_options
+    parser = Trollop::Parser.new do
+        version "Amazon S3 Photo Management Tools 0.5.0 (c) 2013 Christopher Frederick"
+
+        banner <<-EOS
+This program shows a photo's Exif data.
+
+Usage:
+    view_exif_data.rb [options]
+
+Options:
+        EOS
+
+        opt :file, "Photo with Exif data to view",
+            :type => :string, :required => true
+
+    end
+
+    Trollop::with_standard_exception_handling parser do
+        raise Trollop::HelpNeeded if ARGV.empty? # show help
+        parser.parse ARGV
+    end
+end
+
+opts = process_options
 
 ################################################################################
 # MAIN
 ################################################################################
-
-# Process command-line options
-opts = process_options
 
 exit 1 unless opts[:file]
 
