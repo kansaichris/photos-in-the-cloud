5aa60a5332637bfc7eab1bae43e9e6506afcffae
Christopher Frederick <chris@chrisfrederick.net>
2013-07-29 15:23:23 +0900

    Add comments to make_thumbnail.rb

--- make_thumbnail.rb
+++ make_thumbnail.rb
@@ -40,10 +40,12 @@ opts = process_options
 # MAIN
 ###############################################################################
 
+# Get the file path, base file name, and extension
 file_path = opts[:file]
 extension = File.extname(file_path)
 base_name = File.basename(file_path, extension)
 
+# Specify the thumbnail sizes to generate
 thumbnail_sizes = [
   [100, 100],   # Tiny thumbnails
   [150, 150],   # Large thumbnails
@@ -55,18 +57,30 @@ thumbnail_sizes = [
   [1600, 1200]  # X3-large iages
 ]
 
+# Create the 'thumbnails' directory unless it already exists
 Dir.mkdir("thumbnails") unless Dir.exists?("thumbnails")
 
+# Read the original image into memory
 image = Image.read(file_path).first
 
+# Generate a thumbnail for each of the specified dimensions
 thumbnail_sizes.each do |width, height|
+  # Try to resize the image to the specified width and height while
+  # maintaining its aspect ratio. The values passed into the block
+  # through 'cols' and 'rows' may be slightly different from the
+  # specified dimensions to ensure that the image maintains its
+  # aspect ratio AND is no larger than the specified size.
   thumbnail = image.change_geometry("#{width}x#{height}") do |cols, rows, img|
     img.resize(cols, rows)
   end
   # thumbnail = image.resize_to_fill(width, height)
   thumbnail.write("thumbnails/#{base_name}-#{thumbnail.columns}x#{thumbnail.rows}#{extension}")
 
+  # Force RMagick to free the thumbnail's image memory
+  # For more details, see https://github.com/rmagick/rmagick/issues/12
   thumbnail.destroy!
 end
 
+# Force RMagick to free the original file's image memory
+# For more details, see https://github.com/rmagick/rmagick/issues/12
 image.destroy!
